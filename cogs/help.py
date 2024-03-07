d5b46d0b09802d0370e5f72e68c6be0074af30e0
RRRGR <82594900+RRRGR@users.noreply.github.com>
2024-03-07 16:35:07 +0100

    add help for difficulty input

--- cogs/help.py
+++ cogs/help.py
@@ -23,8 +23,10 @@ class Help(commands.Cog):
             name=self.bot.user, icon_url=self.bot.user.display_avatar.url
         )
         text_how_to_submit = "'/ir'とタイプすると'/ir submit'というサジェストが出現します。そのサジェストを押すと提出のための入力欄が現れるので、順に選択肢からの選択、スコア入力、リザルト画像の添付を行って送信してください。Discordをアップデートしていないとサジェストが出ない場合があります。"
+        text_difficulty = "難易度入力は最も難しい難易度を1として入力してください。例えばSDVXの曲の難易度が17/15/12/6から選べる場合、それぞれ入力するべき難易度は1/2/3/4となります。"
         text_bug = "不具合を見つけた場合はGrGurutoに報告をお願いします。"
         help_embed.add_field(name="提出方法", value=text_how_to_submit)
+        help_embed.add_field(name="難易度入力について", value=text_difficulty)
         help_embed.add_field(name="不具合", value=text_bug)
         await interaction.response.send_message(embed=help_embed)
 
